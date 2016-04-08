049a5e4bdaffe3a7442eb0c0da354c9692db14be
bin <1061015041@qq.com>
2016-04-08 14:31:39 +0800

    更新安卓配置

--- public/js/app.js
+++ public/js/app.js
@@ -25,19 +25,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services',  
 
     .config(function($stateProvider, $urlRouterProvider,$ionicConfigProvider) {
       //这里对android进行一些配置,为了保证ios和安卓平台显示效果一致
-      $ionicConfigProvider.platform.ios.tabs.style('standard');
-      $ionicConfigProvider.platform.ios.tabs.position('bottom');
-      $ionicConfigProvider.platform.android.tabs.style('standard');
-      $ionicConfigProvider.platform.android.tabs.position('standard');
-
-      $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
-      $ionicConfigProvider.platform.android.navBar.alignTitle('left');
-
-      $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
-      $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
-
-      $ionicConfigProvider.platform.ios.views.transition('ios');
-      $ionicConfigProvider.platform.android.views.transition('android');
+      $ionicConfigProvider.tabs.position('bottom');
+      $ionicConfigProvider.tabs.style('standard');
+      $ionicConfigProvider.navBar.alignTitle('center');
+      $ionicConfigProvider.navBar.positionPrimaryButtons('left');
+      $ionicConfigProvider.backButton.icon('ion-ios-arrow-left');
+      $ionicConfigProvider.views.swipeBackEnabled(false);
 
 
       $stateProvider
