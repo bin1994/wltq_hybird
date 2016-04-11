ccbc7e10db15507ef767cafb658ae37875da2436
bin <1061015041@qq.com>
2016-04-11 22:02:16 +0800

    新增 公告列表页从服务器获取数据

--- public/js/notice/notice.list.js
+++ public/js/notice/notice.list.js
@@ -15,15 +15,40 @@
     $scope.init = init;
     $scope.selectCat=selectCat;
     $scope.category=1;
-
-
+    $scope.queryNotice1=queryNotice1;
+    $scope.queryNotice2=queryNotice2;
     init();
-
     function init() {
+      queryNotice1();
+      queryNotice2();
     }
 
     function selectCat(cat){
       $scope.category=cat;
+    }
+
+    function queryNotice1(){
+    var query = new AV.Query('notice');
+    query.equalTo('attribute', '1');
+    query.find().then(function(items) {
+        $scope.items=items;
+        $scope.$digest();
+      console.log(items);
+    }, function(error) {
+      console.log('Error: ' + error.code + ' ' + error.message);
+    });
+    }
+
+    function queryNotice2(){
+      var query = new AV.Query('notice');
+      query.equalTo('attribute', '2');
+      query.find().then(function(data) {
+        $scope.data=data;
+        $scope.$digest();
+        console.log(data);
+      }, function(error) {
+        console.log('Error: ' + error.code + ' ' + error.message);
+      });
     }
 
   }
